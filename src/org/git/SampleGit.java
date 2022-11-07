acb1b036b7a2cff59145e0676da9eba92c98af19
Selvavigneshv <selvavigneshmech118@gmail.com>
2022-11-07 19:53:23 +0530

    selvatask

---
+++ src/org/git/SampleGit.java
@@ -0,0 +1,22 @@
+package org.git;
+
+public class SampleGit {
+	
+	public void method1() {
+   System.out.println("java");
+	}
+
+	public void method2() {
+   System.out.println("selenium");
+	}
+
+	public void method3() {
+   System.out.println("sql");
+	}
+public static void main(String[] args) {
+	SampleGit s = new SampleGit();
+	s.method1();
+	s.method2();
+	s.method3();
+}
+}
